bd1804c1153f254f56df2c6300c9334152d4ec6d
IlijaPopovic <60114006+IlijaPopovic@users.noreply.github.com>
2024-06-14 18:22:25 +0200

    links and data updated

--- src/elements/PopUp.js
+++ src/elements/PopUp.js
@@ -1,15 +1,25 @@
 import "./App2D.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleLeft } from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const PopUp = (props) => {
   const childClick = (event) => {
     event.stopPropagation();
   };
 
+  const gitLink = <a href={props.content.gitLink}>GitHub</a>;
+
   const body = props.content.body.map((item, index) => (
     <div key={index}>
-      <h3>{item.subTitle}</h3>
+      <h3>
+        {item.subTitle}{" "}
+        {item.gitLink && (
+          <a href={item.gitLink} target="_blank">
+            <FontAwesomeIcon icon={faGithub} className="icon" />
+          </a>
+        )}
+      </h3>
       <p>{item.text}</p>
     </div>
   ));
